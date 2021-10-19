94de29dbb363a963e6306cf130611da381b87d55
Max Coursey <mrc30363@gmail.com>
2021-10-18 22:10:14 -0400

    "systemd service working, pubexec, startpipeline working - no cloud logging pubexec tho"

--- gcp_forkPyThenSendPub/pubwatch_executor.py
+++ gcp_forkPyThenSendPub/pubwatch_executor.py
@@ -21,43 +21,41 @@ watch for published TOPIC within a PROJECT (updated by dev)
 import logging
 import os
 import sys
-
-from cloud_handler import CloudLoggingHandler
 from cron_executor import Executor
-
+#from cloud_handler import CloudLoggingHandler
 
 # TODO - update PROJECT, TOPIC (start docker image pipeline), STARTAPP (fork/waits py containers)
-PROJECT = 'myinstancepubsubapp'  # change this to match your project
+PROJECT = 'myinstancepubsubapp'
 TOPIC = 'start-ce-pipeline'
-STARTAPP = 'start_pipeline_task.py'
+STARTAPP = '/home/mxmcoursey/gcp_forkPyThenSendPub/start_pipeline_task.py'
 
 script_path = os.path.abspath(os.path.join(os.getcwd(), STARTAPP))
+print("[TRIGGER] {} - Triggered by Topic, {}".format(STARTAPP, TOPIC))
 
-#unbuffer - not sure it matters though
 start_pipeline = "python -u %s" % script_path
 
-
-root_logger = logging.getLogger('cron_executor')
-root_logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler(sys.stderr)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root_logger.addHandler(ch)
-
-cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
-root_logger.addHandler(cloud_handler)
+# TODO - removed - GCP boilerplate cloud_handler.py is outdated NOT working
+# root_logger = logging.getLogger('cron_executor')
+# root_logger.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler(sys.stderr)
+# ch.setLevel(logging.DEBUG)
+# formatter = logging.Formatter(
+#     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# ch.setFormatter(formatter)
+# root_logger.addHandler(ch)
+#
+# cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
+# root_logger.addHandler(cloud_handler)
 
 # create the executor that watches the topic, and will run the job task
 pub_executor = Executor(topic=TOPIC, project=PROJECT, task_cmd=start_pipeline, subname='start_pipeline_task')
 
 # add a cloud logging handler and stderr logging handler
-job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=pub_executor.subname)
-pub_executor.job_log.addHandler(job_cloud_handler)
-pub_executor.job_log.addHandler(ch)
-pub_executor.job_log.setLevel(logging.DEBUG)
-
+# job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=pub_executor.subname)
+# pub_executor.job_log.addHandler(job_cloud_handler)
+# pub_executor.job_log.addHandler(ch)
+# pub_executor.job_log.setLevel(logging.DEBUG)
 
 # watches indefinitely
+print("[PUBWATCH] Watching Topic: {}".format(TOPIC))
 pub_executor.watch_topic()
